19e4256e6d660e65cc569c1d2da54042d1e3cf5e
Tamás Németh <tomuwhu@gmail.com>
2018-04-23 10:44:18 +0200

    .

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 angular
   .module('a', [])
   .controller('c', ($scope,$window) => {
-        $scope.x=1
-        $scope.y=1
-        lnko = (a,b) => {
-            if (a == b) return a
-            if (a > b) return lnko(a-b,b)
-            else return lnko(a,b-a)
-        }
-        $scope.f = () =>
-            $scope.er=lnko(parseInt($scope.x), parseInt($scope.y))
-        $scope.f()    
-        $window.document.getElementById('i').focus()
+        
   } )
