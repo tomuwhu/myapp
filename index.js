ea1fa3018275b8cf22c5d875867cf26f3e1e03b4
Tamás Németh <tomuwhu@gmail.com>
2018-04-23 11:13:30 +0200

    .

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 angular
   .module('a', [])
   .controller('c', ($scope,$window) => {
-        
+    $scope.lista = [ ]
+    $scope.hozzaad = () => {
+      $scope.lista.push($scope.ef)
+      $scope.lista.sort( (a,b) => a.localeCompare(b) )
+    }
   } )
